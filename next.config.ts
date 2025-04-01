115668ecce872a8c7e11a0f008d3d66fc9889c7a
M2 C1 <hienphannguyenxuan@gmail.com>
2025-04-01 15:47:05 +0700

    new order10

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,32 @@
-import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 import withBundleAnalyzer from '@next/bundle-analyzer';
+
+// Khởi tạo plugin next-intl
 const withNextIntl = createNextIntlPlugin();
 
+// Cấu hình Bundle Analyzer
 const withAnalyzer = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// Cấu hình Next.js
+const nextConfig = {
   experimental: {
-    optimizePackageImports:['@mantine/core', '@mantine/hooks', '@mantine/charts']
+    optimizePackageImports: ['@mantine/core', '@mantine/hooks', '@mantine/charts'],
   },
-  
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(withAnalyzer(nextConfig));
