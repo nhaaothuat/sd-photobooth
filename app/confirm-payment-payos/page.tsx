96feeee9602e4b983593f81fc02a222e85849a8b
M2 C1 <hienphannguyenxuan@gmail.com>
2025-04-01 13:09:53 +0700

    new order5

--- app/confirm-payment-payos/page.tsx
+++ app/confirm-payment-payos/page.tsx
@@ -4,26 +4,9 @@ import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const PaymentConfirm = () => {
-  const searchParams = useSearchParams();
-  const [orderCode, setOrderCode] = useState("");
-  const [status, setStatus] = useState("");
-  const [cancel, setCancel] = useState("");
-
-  useEffect(() => {
-    setOrderCode(searchParams.get("ordercode") || "");
-    setStatus(searchParams.get("status") || "");
-    setCancel(searchParams.get("cancel") || "");
-  }, [searchParams]);
-
+ 
   return (
-    <div className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Thông tin thanh toán</h1>
-      <div className="bg-white p-4 shadow-md rounded-md">
-        <p><strong>Order Code:</strong> {orderCode}</p>
-        <p><strong>Status:</strong> {status}</p>
-        <p><strong>Cancel:</strong> {cancel}</p>
-      </div>
-    </div>
+   <h1>Hello</h1>
   );
 };
 
